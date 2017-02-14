415991845c661323b7dcd16ef036e566c69f3b9b
insta-code1 <eoinkane1@gmail.com>
2017-02-14 18:14:35 +0000

    Fetching longitude and latitude

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ request({
   json: true
 }, (error, response, body) => {
   console.log(`Response: ${JSON.stringify(response, undefined, 3)}`);
+  let latitude = body.results[0].geometry.location.lat;
+  let longitude = body.results[0].geometry.location.lng;
 });
