793861885c22180544a0ddc88cf5287c92638a1e
insta-code1 <eoinkane1@gmail.com>
2017-02-14 18:54:32 +0000

    Handling our Geolocation request errors

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const request = require('request');
 
 request({ 
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=dublin+ireland',
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=0000000000000',
   json: true
 }, (error, response, body) => {
-  console.log(`Response: ${JSON.stringify(response, undefined, 3)}`);
-  let latitude = body.results[0].geometry.location.lat;
-  let longitude = body.results[0].geometry.location.lng;
+
+  if (error) {
+    console.log('Error while fetching data');
+  } else if (body.status === "ZERO_RESULTS") {
+    console.log('Unable to find address');
+  } else if (body.status === "OK") {
+    let latitude = body.results[0].geometry.location.lat;
+    let longitude = body.results[0].geometry.location.lng;
+    console.log(`longitude: ${longitude} and latitude: ${latitude}`);
+  }
 });
