562e3cc21c624e0599cca430ed2d442bf8d8cfb1
insta-code1 <eoinkane1@gmail.com>
2017-02-15 13:36:55 +0000

    fetching weather data and ignoring api keys

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const request = require('request');
+const appid = require('./weather_api_key');
 
 request({ 
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=0000000000000',
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=dublin+ireland',
   json: true
 }, (error, response, body) => {
 
@@ -13,5 +14,31 @@ request({
     let latitude = body.results[0].geometry.location.lat;
     let longitude = body.results[0].geometry.location.lng;
     console.log(`longitude: ${longitude} and latitude: ${latitude}`);
+   fetchWeatherData(latitude, longitude);
   }
 });
+
+let fetchWeatherData = (lat, log) => {
+
+  request({
+    url:`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${appid}`,
+    json: true
+  }, (error, response, body) => {
+
+    if (error) {
+      console.log('unable to fetch data');
+    } else if (body.weather == null) {
+      console.log('unable to find weather data for this address');
+    } else if (response.statusCode == 200 && body.weather) {
+      let weatherDataObj = {
+        description: body.weather[0].description,
+        temp: body.main.temp,
+        windSpeed: body.wind.speed
+      };
+      console.log(`weatherDataObj: ${weatherDataObj.temp}`);
+
+    }
+
+    //  console.log(`Response: ${JSON.stringify(response, undefined, 3)}`);
+  });
+}
