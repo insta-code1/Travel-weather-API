245007fde426bafef67cb0aa6a0bbe677773dbd0
insta-code1 <eoinkane1@gmail.com>
2017-02-16 16:48:26 +0000

    Rendering weather data on table

--- public/js/script.js
+++ public/js/script.js
@@ -10,14 +10,28 @@ $('#searchButton').on('click', function(e) {
    url: "/api/weatherdata/",
    data: location,
    success: function(data) {
-     $('#toggle').removeClass('hidden');
-     console.log(JSON.stringify(data));
+     success(data);
    },
    error: function() {
      alert('error');
    }
+  }); // $.ajax call
 
- }); // $.ajax call
+
+  function success(serverData) {
+      $('#toggle').removeClass('hidden');
+    console.log(JSON.stringify(serverData));
+
+    var wind = serverData.data.windSpeed;
+    var clouds = serverData.data.description;
+    var temp = serverData.data.temp;
+
+    document.querySelector('#wind').innerHTML = wind;
+    document.querySelector('#clouds').innerHTML = clouds;
+    document.querySelector('#temp').innerHTML = temp;
+
+  }
+
 
 
 }); // close button event
