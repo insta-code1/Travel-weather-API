22fa2222857a7f825c630d23cfceb7ac040fe8ca
insta-code1 <eoinkane1@gmail.com>
2017-02-16 13:39:07 +0000

    Added Ajax request

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,21 @@ $('#searchButton').on('click', function(e) {
 
   e.preventDefault();
 
-  alert('It works!!');
+ var userInput = $('#inputValue').val();
+ var location = {"location" : userInput };
+
+ $.ajax({
+   type: "POST",
+   url: "/api/weatherdata",
+   data: location,
+   success: function() {
+     alert(location);
+   },
+   error: function() {
+     alret('error');
+   }
+
+ }); // $.ajax call
+
 
 }); // close button event
